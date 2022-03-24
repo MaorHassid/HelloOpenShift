fe7fb55a6cb4cc3c5be0e93b6cea552abecdf135
MaorHassid <maorhassid@gmail.com>
2022-03-24 16:07:47 +0200

    Add Serilog

--- HelloOpenshift/Program.cs
+++ HelloOpenshift/Program.cs
@@ -1,4 +1,16 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Initial serilog
+builder.Host.UseSerilog();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+    .MinimumLevel.Override("System", Serilog.Events.LogEventLevel.Information)
+    .Enrich.WithMachineName()
+    .WriteTo.Console()
+    .CreateLogger();
 
 var app = builder.Build();
 
@@ -8,8 +20,13 @@ app.UseHttpsRedirection();
 // Add HTTP GET entry point.
 app.MapGet("/", () =>
 {
+    string secretMessage = builder.Configuration["SECRET_MESSAGE"];
+
+    Log.Information($"Message: {secretMessage}");
+
     return "Welcome aboard! This project was created only for educational purposes.";
 });
 
+Log.Information("Starting application");
 // Start running the host.
 app.Run();
